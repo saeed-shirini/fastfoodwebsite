529afbace1b2c7ada42cd93b933cd0efc1143974
saeed <saeedshirini1@gmail.com>
2024-07-09 07:32:57 -0700

    final

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import FastFoodList from "./components/fastfood/FastFoodList";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
